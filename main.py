c467bb7b31674dabe670fa18a639e0d781e83764
Franzuq <fchew00@hotmail.se>
2024-10-16 15:19:07 +0200

    lagt till felhantering om spelaren skriver fel och lagt till funktionen checkIfTie när board är ifyllt

--- main.py
+++ main.py
@@ -39,15 +39,30 @@ def checkWinner(player):
         print(player + " vann spelet, spelet avslutas nu ") #här säger spelet när det har slutats
         sys.exit() #den här funktionen avslutar programmet
 
+#det här är funktionen för att se om spelet blir oavgjort
+def checkIfTie(board):
+    tie = all(value != " " for value in board.values()) #vi checkar om alla values inte är like med blank space och vi får tillbaka en boolean 
+    #tie blir en boolean
+    if (tie): #om tie är true blir det oavgjort
+        printBoard(theBoard)
+        print("Det blev oavgjort, spelet avslutas nu")
+        sys.exit() 
+
 
 #här skapade jag spelaren  
 playerMark = "x"
 #här är loopen för spelet 
-for i in range(9):
+for i in range(9999):
     printBoard(theBoard)
     print("Det är " + playerMark + " tur")
     move = input() #här frågar jag spelarens input och sparar den i en value
+    if move in theBoard and theBoard[move] == " ": #jag checkar om spelarens input är faktiskt en key i vår board och om den key inte redan är ifylld 
+        theBoard[move] = playerMark 
+    else: #om key inte finns eller redan är ifylld printas ett meddelande 
+        print("Platsen du skrev finns inte eller är redan ifylld")
+        continue #börjar om loopen
     theBoard[move] = playerMark #här ersätter jag en av bordens value med en mark 
+    checkIfTie(theBoard)
     if playerMark == "x": #om spelarens mark är lika med x 
         checkWinner(playerMark)
         playerMark = "o" #spelarens mark blir o
