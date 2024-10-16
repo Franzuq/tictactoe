f0b0ed5616fcf5d77eb17d7bf3af955e860e73dd
Franzuq <fchew00@hotmail.se>
2024-10-16 14:28:38 +0200

    skapade funktionen för att se vem som vinner och lagt till den i spelets loop

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys #här importeras funktionen exit  
+
 #det här en library, jag satte varje value som blank space 
 #under spelets gång kommer blank spaces ändras till X eller O 
 theBoard = {
@@ -7,9 +9,9 @@ theBoard = {
     "mid-L": " ",
     "mid-M": " ",
     "mid-R": " ",
-    "low-L": " ",
-    "low-M": " ",
-    "low-R": " ",
+    "bot-L": " ",
+    "bot-M": " ",
+    "bot-R": " ",
 }
 
 #det här är funktionen för att printa board
@@ -18,7 +20,25 @@ def printBoard(board):
     print("--+-+--")
     print(" " + board["mid-L"] + "|" + board["mid-M"] + "|" + board["mid-R"])
     print("--+-+--")
-    print(" " + board["low-L"] + "|" + board["low-M"] + "|" + board["low-R"])
+    print(" " + board["bot-L"] + "|" + board["bot-M"] + "|" + board["bot-R"])
+
+#det här funktionen för att se när spelaren vinner
+#här checkar vi om playerMark finns på alla möjliga ställen där man kan vinna 
+def checkWinner(player):
+    if (
+        (player == theBoard["top-L"] == theBoard["top-M"] == theBoard["top-R"])
+        or (player == theBoard["mid-L"] == theBoard["mid-M"] == theBoard["mid-R"])
+        or (player == theBoard["bot-L"] == theBoard["bot-M"] == theBoard["bot-R"])
+        or (player == theBoard["top-L"] == theBoard["mid-L"] == theBoard["bot-L"])
+        or (player == theBoard["top-M"] == theBoard["mid-M"] == theBoard["bot-M"])
+        or (player == theBoard["top-R"] == theBoard["mid-R"] == theBoard["bot-R"])
+        or (player == theBoard["top-L"] == theBoard["mid-M"] == theBoard["bot-R"])
+        or (player == theBoard["top-R"] == theBoard["mid-M"] == theBoard["bot-L"])
+    ):
+        printBoard(theBoard)
+        print(player + " vann spelet, spelet avslutas nu ") #här säger spelet när det har slutats
+        sys.exit() #den här funktionen avslutar programmet
+
 
 #här skapade jag spelaren  
 playerMark = "x"
@@ -29,6 +49,9 @@ for i in range(9):
     move = input() #här frågar jag spelarens input och sparar den i en value
     theBoard[move] = playerMark #här ersätter jag en av bordens value med en mark 
     if playerMark == "x": #om spelarens mark är lika med x 
+        checkWinner(playerMark)
         playerMark = "o" #spelarens mark blir o
-    else: #om spelarens mark inte är lika med x 
+    else: #om spelarens mark inte är lika med x
+        checkWinner(playerMark) 
         playerMark = "x" #spelarens mark blir x
+
